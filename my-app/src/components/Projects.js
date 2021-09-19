898fbf932505ce41bdce67bd78829a8f67f0d8c7
guinhoanjos <iago.anjos@hotmail.com>
2021-09-19 20:38:29 -0300

    Update Projects.js

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Projects(){
   return(
-    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif"></img>
+    <img alt="gif"src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif"></img>
   )
 }
