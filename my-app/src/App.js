dfd98f822e5129f5d3b580e33d8fe700b093ac68
guinhoanjos <iago.anjos@hotmail.com>
2021-09-19 20:44:02 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
 import './stack.css';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/home';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>{Home}</Route>
-      </Switch>
-    </Router>
+    <Home></Home>
   );
 }
 
