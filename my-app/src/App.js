5fcbacf3b6d266105dc74e003cb60e8ee21e00c0
guinhoanjos <iago.anjos@hotmail.com>
2021-09-20 12:50:49 -0300

    criado rota

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import './stack.css';
 import Home from './pages/home';
-
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 function App() {
   return (
-    <Home></Home>
+    <BrowserRouter>
+      <Switch>
+        <Route  path='/ghpages' component={Home}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
